d05f4070221066e7f32a5989f52eb85c99eb5381
DavMunHer <davidmunyozh@gmail.com>
2025-10-09 12:41:22 +0200

    feat: result sets and storing data in a list

--- JDBC/src/main/java/org/example/JDBCMain.java
+++ JDBC/src/main/java/org/example/JDBCMain.java
@@ -3,13 +3,18 @@ package org.example;
 import io.github.cdimascio.dotenv.Dotenv;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JDBCMain {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.configure().directory("./JDBC").load();
-            Statement st = null;
+        Statement st = null;
+        Connection cn = null;
+        ResultSet rs = null;
+        List<Account> accounts = new ArrayList<>();
         try {
-            Connection cn = DriverManager.getConnection(dotenv.get("DB_URL"), dotenv.get("DB_USER"), dotenv.get("DB_PASSWORD"));
+            cn = DriverManager.getConnection(dotenv.get("DB_URL"), dotenv.get("DB_USER"), dotenv.get("DB_PASSWORD"));
             st = cn.createStatement();
             st.executeUpdate("DROP DATABASE IF EXISTS bank;");
             st.executeUpdate("CREATE DATABASE IF NOT EXISTS bank;");
@@ -26,13 +31,41 @@ public class JDBCMain {
             ps.setInt(3, 100);
             ps.execute();
 
+            rs = st.executeQuery("SELECT * FROM Account;");
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String code = rs.getString("code");
+                int balance = rs.getInt("balance");
+                Account ac = new Account(id, code, balance);
+                accounts.add(ac);
+            }
+
+            accounts.forEach(System.out::println);
+
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
 
+            if (cn != null) {
+                try {
+                    cn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
             if (st != null) {
                 try {
                     st.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (rs !=  null) {
+                try {
+                    rs.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
