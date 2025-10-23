b1add7e1017d18f34e2063a66e72261760e2691a
DavMunHer <davidmunyozh@gmail.com>
2025-10-23 11:16:07 +0200

    feat: added basic configuration for hibernate

---
+++ Hibernate/src/main/java/org/example/entities/Cliente.java
@@ -0,0 +1,85 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", length = 30, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "edad")
+    private Integer edad;
+
+    public Cliente() {}
+
+    public Cliente(Long id, String firstName, String lastName, String email, Integer edad) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+}
